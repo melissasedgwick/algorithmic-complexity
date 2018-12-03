666d051aa7236671998adf7576dd705572237c58
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-12-03 12:12:29 +0000

    Added timings solution with method

---
+++ timings/timings/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace timings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer();
+        }
+
+        static List<int> GetRandomList(int length)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < length; i++)
+            {
+                Random rand = new Random();
+                list.Add(rand.Next(10));
+            }
+
+            return list;
+        }
+
+        static void Timer()
+        {
+            List<double> list = new List<double>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var listOfNumbers = GetRandomList(10000);
+
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                //METHOD HERE
+
+                stopWatch.Stop();
+                list.Add(stopWatch.Elapsed.TotalMilliseconds);
+            }
+
+            Console.WriteLine("Times to execute:");
+
+            foreach (var time in list)
+            {
+                Console.WriteLine("{0}ms", time);
+            }
+        }
+    }
+}
