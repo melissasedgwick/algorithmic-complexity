9b14f2f86328200b542ae9589524d75f7ab96da7
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-12-05 15:08:55 +0000

    Refactored timings program

--- timings/timings/Program.cs
+++ timings/timings/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Collections.Generic;
 
 namespace timings
 {
@@ -11,25 +10,14 @@ namespace timings
             Timer();
         }
 
-        static List<int> GetRandomList(int length)
-        {
-            List<int> list = new List<int>();
-            for (int i = 0; i < length; i++)
-            {
-                Random rand = new Random();
-                list.Add(rand.Next(10));
-            }
-            return list;
-        }
-
         static void Timer()
         {
-            List<double> list = new List<double>();
             Console.WriteLine("Times to execute:");
 
             for (int i = 0; i < 10; i++)
             {
-                var listOfNumbers = GetRandomList(20000);
+                var listOfNumbers = RandomListsAndArrays.GetRandomList(20000);
+                var arrayOfNumbers = RandomListsAndArrays.GetRandomArray(20000);
                 Stopwatch stopWatch = new Stopwatch();
                 stopWatch.Start();
 
@@ -38,20 +26,6 @@ namespace timings
                 stopWatch.Stop();
                 Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
             }
-        }
-
-        static List<E> ShuffleList<E>(List<E> inputList)
-        {
-            List<E> randomList = new List<E>();
-            Random r = new Random();
-            int randomIndex = 0;
-            while (inputList.Count > 0)
-            {
-                randomIndex = r.Next(0, inputList.Count);
-                randomList.Add(inputList[randomIndex]);
-                inputList.RemoveAt(randomIndex);
-            }
-            return randomList;
         }
     }
 }
---
+++ timings/timings/RandomListsAndArrays.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace timings
+{
+    public class RandomListsAndArrays
+    {
+
+        public static List<int> GetRandomList(int length)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < length; i++)
+            {
+                Random rand = new Random();
+                list.Add(rand.Next(10));
+            }
+            return list;
+        }
+
+        public static int[] GetRandomArray(int length)
+        {
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                Random rand = new Random();
+                array[i] = rand.Next(10);
+            }
+            return array;
+        }
+    }
+}
